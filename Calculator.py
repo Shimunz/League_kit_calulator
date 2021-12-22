6f071425c76269e5d3153f689b0ee4ba10e36569
Shimunz <simonzhao@outlook.com>
2021-12-22 23:56:07 +1300

    testing getChampionName

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,13 @@
+import json
 from Champion import Abilities, Champion
 from AbilityType import *
+from JsonExtractor import *
+
+f = open('Aatrox.json')
+fdata = json.load(f)
+
+champion_name = Extractor.getChampionName(fdata)
+print(champion_name)
 
 # f = open("Test.txt")
 
@@ -14,33 +22,33 @@ from AbilityType import *
 # def reader(line):
 #     return line
 
-ability_properties = []
-ab_list = []
+# ability_properties = []
+# ab_list = []
 
-a = AbilityType("AP", [55,95,135,175,215], 0.6, 12)
-b = AbilityType("AD", [5,9,13,17,21], 1, 12)
+# a = AbilityType("AP", [55,95,135,175,215], 0.6, 12)
+# b = AbilityType("AD", [5,9,13,17,21], 1, 12)
 
-ability_properties.append(a)
-ability_properties.append(b)
+# ability_properties.append(a)
+# ability_properties.append(b)
 
-ablity = Abilities("soul", ability_properties, 10)
-ab_list.append(ablity)
+# ablity = Abilities("soul", ability_properties, 10)
+# ab_list.append(ablity)
 
-ability_properties.clear()
+# ability_properties.clear()
 
-a = AbilityType("AD", [10,20,30,40,50], 0.7, 0)
+# a = AbilityType("AD", [10,20,30,40,50], 0.7, 0)
 
-ability_properties.append(a)
+# ability_properties.append(a)
 
-ablity = Abilities("ass", ability_properties, 5)
-ab_list.append(ablity)
+# ablity = Abilities("ass", ability_properties, 5)
+# ab_list.append(ablity)
 
-base_stats = [500, 40, 40, 70, 0, 330, 0] #health, ar, mr, ad, ap, speed tenacity
+# base_stats = [500, 40, 40, 70, 0, 330, 0] #health, ar, mr, ad, ap, speed tenacity
 
-nasus = Champion("nasus", base_stats, ab_list)
+# nasus = Champion("nasus", base_stats, ab_list)
 
 
-print(nasus.champion_name, nasus.base_stats, nasus.abilities)
+# print(nasus.champion_name, nasus.base_stats, nasus.abilities)
 
-for x in range(len(nasus.abilities)):
-    print(nasus.abilities[x])
+# for x in range(len(nasus.abilities)):
+#     print(nasus.abilities[x])
