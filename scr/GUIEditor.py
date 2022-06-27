42939f5a69cb8f1494fca57ef6b212808806dbed
Shimunz <simonzhao@outlook.com>
2022-06-27 15:35:05 +1200

    added in the ability to search

--- scr/GUIEditor.py
+++ scr/GUIEditor.py
@@ -1,3 +1,4 @@
+from pickle import NONE
 import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showinfo
@@ -5,21 +6,34 @@ from DAO.ChampionDAO import *
 
 
 class EditorFrame(tk.Toplevel):
+
+    champion_list = ChampionDAO.all_champion_names
+
     def __init__(self, container):
         super().__init__(container)
+
+        self.champion_selected = tk.StringVar()
+        self.champion_selected.trace('w', self.update)
+
+        self.comboBox = ttk.Combobox(self, textvariable=self.champion_selected)
 
         self.searchbox = self.create_select_champion_widget()
 
         self.tree = self.create_tree_widget()
     
+    #auto updates the combox depending on user input
+    #TODO: Make searching not case sensitive.
+    def update(self, *args):
+        newvalues=[i for i in self.champion_list if self.champion_selected.get() in i]
+        self.comboBox['values']=newvalues
+
     def create_select_champion_widget(self):
         self.label = ttk.Label(self, text="Search Champion")
         #self.label.pack(side="left")
         self.label.grid(column=0, row=0)
 
-        current_var = tk.StringVar()
-        self.comboBox = ttk.Combobox(self, textvariable=current_var)
-        self.comboBox['values'] = ChampionDAO.all_champion_names
+        
+        self.comboBox['values'] = self.champion_list
         self.comboBox.grid(column=1, row=0)
         #self.comboBox.pack(side="left")
 
