7038fbef3115edcf23c2e1011851e6fd7d6757e2
Shimunz <simonzhao@outlook.com>
2022-06-29 16:43:57 +1200

    added the ability to view champions stats in the tree view

--- scr/GUIEditor.py
+++ scr/GUIEditor.py
@@ -17,28 +17,31 @@ class EditorFrame(tk.Toplevel):
         self.champion_selected = tk.StringVar()
         self.champion_selected.trace('w', self.update)
 
+        self.btn_update_tree=ttk.Button(self,
+                text='Ok',
+                command=lambda:self.update_Tree())
+
+
         self.comboBox = ttk.Combobox(self, textvariable=self.champion_selected)
         
         self.create_select_champion_widget()
         self.tree = self.create_tree_widget()
         
-    
-    #auto updates the combox depending on user input
-    #TODO: Make searching not case sensitive.
-    def update(self, *args):
-        newvalues=[i for i in self.champion_list if self.champion_selected.get() in i]
-        self.comboBox['values']=newvalues
-
+        
     def create_select_champion_widget(self):
         self.label = ttk.Label(self, text="Search Champion")
         #self.label.pack(side="left")
         self.label.grid(column=0, row=0)
 
-        
         self.comboBox['values'] = self.champion_list
         self.comboBox.grid(column=1, row=0)
+
+        self.btn_update_tree.grid(column=2, row=0)
+
         #self.comboBox.pack(side="left")
 
+    #Creates the tree
+    #TODO make tree be empty. only fill tree when someone selected champion
     def create_tree_widget(self):
         champion_list = ChampionDAO.champion_list
         columns = ('attribute', 'value')
@@ -60,19 +63,7 @@ class EditorFrame(tk.Toplevel):
 
         tree.configure(yscrollcommand=scrollbar.set)
 
-        champ_stats = []
-
-        for champs in ChampionDAO.champion_list:
-            if champs.champion_properties['champion_name']=="Aatrox":
-                champ = champs.champion_properties['stats'].total_stats
-            break                    
-
-        for c in champ:
-            print (c)
-            champ_stats.append((c, champ[c]))
-
-        for i in champ_stats:
-            tree.insert('', tk.END, values=i)        
+                
             
         return tree
 
@@ -83,4 +74,27 @@ class EditorFrame(tk.Toplevel):
             # show a message
             showinfo(title='Information', message=','.join(record))
 
+    #auto updates the combox depending on user input
+    #TODO: Make searching not case sensitive.
+    def update(self, *args):
+        newvalues=[i for i in self.champion_list if self.champion_selected.get() in i]
+        self.comboBox['values']=newvalues
+
+    def update_Tree(self):
+        champ_stats = []
+        champ_name = self.champion_selected.get()
+
+        for champs in ChampionDAO.champion_list:
+            print(champs.champion_properties['champion_name'], champ_name)
+            if champs.champion_properties['champion_name'] == champ_name:
+                champ = champs.champion_properties['stats'].total_stats
+                break                    
+
+        for c in champ:
+            print (c)
+            champ_stats.append((c, champ[c]))
+
+        for i in champ_stats:
+            self.tree.insert('', tk.END, values=i)
+        
     
